14a0febe49e100decfc7a7f840c95c86545f7841
森定遼 <seventh.mark.1227.zng@gmail.com>
2016-11-14 17:33:03 -0800

    comittoinit

--- sampleSwitch/ViewController.swift
+++ sampleSwitch/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func changedSwitch(_ sender: AnyObject) {
+        
+        print(sender.isOn)
+        
+        if sender.isOn == true{
+        print ("スイッチオン")
+        }else{
+        print ("スイッチオフ")
+        }
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
